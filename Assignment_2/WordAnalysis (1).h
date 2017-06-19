926d028e232a0885eb54f2179531554225213c1a
stevenkarl <steven.karl@colorado.edu>
2017-06-18 22:02:48 -0600

    Add files via upload

---
+++ Assignment_2/WordAnalysis (1).h
@@ -0,0 +1,24 @@
+struct word {
+	int count;
+	std::string w;
+};
+
+class WordAnalysis{
+	private:
+		int timesDoubled;
+		word *words;
+		int wordCount;
+		int index;
+		void doubleArrayAndAdd(std::string);
+		bool checkIfCommonWord(std::string);
+		void sortData();
+	public:
+		bool readDataFile(char*); //returns an error if file not opened
+		int getWordCount();
+		int getUniqueWordCount();
+		int getArrayDoubling();
+		void printCommonWords(int);
+		void printResult(int);
+		WordAnalysis(int);
+		~WordAnalysis();
+};

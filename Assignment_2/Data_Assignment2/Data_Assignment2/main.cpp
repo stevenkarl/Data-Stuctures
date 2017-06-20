60309ebd671a7b483622021a8ad4d476125eaa60
stevenkarl <steven.karl@colorado.edu>
2017-06-20 17:45:02 -0600

    Add files via upload

--- Assignment_2/Data_Assignment2/Data_Assignment2/main.cpp
+++ Assignment_2/Data_Assignment2/Data_Assignment2/main.cpp
@@ -17,38 +17,40 @@
 
 using namespace std;
 
-void WordAnalysis::doubleArrayAndAdd(word *uniqueWords){
-    word *tempWords;
-    int timesDoubled = 0;
-    int arraySize = 100;
-    int index = 0;
-    int wordCount = 0;
-    if(index == arraySize){
-        tempWords = new word[2*arraySize];
+void WordAnalysis::doubleArrayAndAdd(string nWord){//doi
+    if(index == wordCount){
         
-        for (int i = 0; i < arraySize; i++) {
-            tempWords[i] = uniqueWords[i];
+        word *tempWords = new word[2*wordCount];
+        
+        for (int i = 0; i < wordCount; i++) {
+            tempWords[i] = words[i];
         }
-        delete[] uniqueWords;
-        uniqueWords = new word[2*arraySize];
-        arraySize = 2*arraySize;
-        for(int j = 0; j< arraySize; j++){
-            uniqueWords[j] = tempWords[j];
+        delete[] words;
+        words = new word[2*wordCount];
+        wordCount = 2*wordCount;
+        for(int j = 0; j< wordCount; j++){
+            words[j] = tempWords[j];
         }
         delete[] tempWords;
         
         timesDoubled++;
+        word temp;
+        temp.w = nWord;
+        temp.count = 1;
+        words[index] = temp;
+        return;
+        
     }
+    word temp;
+    temp.w = nWord;
+    temp.count = 1;
+    words[index] = temp;
 }
 
 int WordAnalysis::getArrayDoubling(){
-    int timesDoubled = 0;
-    if(WordAnalysis::doubleArrayAndAdd(<#word *uniqueWords#>)){
-        timesDoubled++;
-    }
-
+   
     return timesDoubled;
-};
+}
 
 void WordAnalysis::sortData(){
     word swap;
@@ -56,33 +58,32 @@ void WordAnalysis::sortData(){
     {
         for(int j = 0; j < (index - i); j++)
         {
-            if(uniqueWords[j].count < uniqueWords[j+1].count)
+            if(words[j].count < words[j+1].count)
             {
-                swap = uniqueWords[j];
-                uniqueWords[j] = uniqueWords[j+1];
-                uniqueWords[j+1] = swap;
+                swap = words[j];
+                words[j] = words[j+1];
+                words[j+1] = swap;
             }
         }
     }
 
 }
 
-void WordAnalysis::printResult(int ){
-    printCommonWords(uniqueWords, printSize);
+void WordAnalysis::printResult(int num){
+    printCommonWords(num);
     cout << "#" << endl;
     cout << "Array doubled :" << timesDoubled << endl;
     cout << "#" << endl;
     cout << "Unique non-common words:" << index << endl;
     cout << "#" << endl;
-    cout << "Total non-common words:" << getWordCount(); << endl;
+    cout << "Total non-common words:" << getWordCount() << endl;
 }
 
-void WordAnalysis::printCommonWords(int topN)
-{
-    word printedArray[topN];
+void WordAnalysis::printCommonWords(int topN){
+    word *printedArray = new word[topN];
     for(int i = 0; i<topN; i++){
-        printedArray[i].w = wordList[i].w;
-        printedArray[i].count = wordList[i].count;
+        printedArray[i].w = words[i].w;
+        printedArray[i].count = words[i].count;
     }
     for(int i = 0; i<topN; i++){
         cout << printedArray[i].w << " - " << printedArray[i].count << endl;
@@ -95,37 +96,20 @@ int WordAnalysis::getWordCount()
     int totalCount = 0;
     for(int i = 0; i<index; i++)
     {
-        totalCount += wordList[i].count;
+        totalCount = totalCount + words[i].count;
     }
     return totalCount;
 }
 
 int WordAnalysis::getUniqueWordCount()
 {
-    bool alreadyInList = false;
-    int index = 0;
-    
-    for(int i = 0; i < index; i++)			//loop through all seen unique words
-    {
-        if(uniqueWordsList[i].w == word)					//if hte new unique word is an already seen unique word
-        {
-            alreadyInList = true;
-            uniqueWordsList[i].count++;
-            return 0;
-        }
-    }
-    if(!alreadyInList)
-    {
-        uniqueWordsList[index].w = word;
-        uniqueWordsList[index].count ++;
-        return 1;
-    }
+
     return index;
 }
 
 
-bool WordAnalysis::checkIfCommonWord(string word)//call this function to make sure not to add these words to the list of common words
-{
+bool WordAnalysis::checkIfCommonWord(string word){//call this function to make sure not to add these words to the list of common words
+
     bool commonWord = false;
     string excludedWords[50] = {"the","be","to","of","and","a","in","that","have","i","it",
         "for","not","on","with","he","as","you","do","at","this","but","his","by","from","they",
@@ -142,12 +126,9 @@ bool WordAnalysis::checkIfCommonWord(string word)//call this function to make su
     return commonWord;
 }
 
-bool WordAnalysis::readDataFile(string fName){//use this function to read the fie
-    bool fileRead = true;
-    word *uniqueWords = new word[100];
-    word *tmpWords;
-    string uniqueLine;
-    string uniqueWord;
+bool WordAnalysis::readDataFile(char *fName){//use this function to read the fie
+    string uniqueLine = "";
+    string uniqueWord = "";
     ifstream infile;
     infile.open(fName);//open the file that the user passes
     if(infile.is_open()){
@@ -158,37 +139,61 @@ bool WordAnalysis::readDataFile(string fName){//use this function to read the fi
             
             //get the individual word from the line
             while(ss >> uniqueWord){
-                WordAnalysis::doubleArrayAndAdd(<#word *uniqueWords#>);
-                if(WordAnalysis::checkIfCommonWord(uniqueWord) == true){
+                if(checkIfCommonWord(uniqueWord) == true){
                     //I DON'T WANT IT IN THE ARRAY
                 }
                 
                 else{
-                    // I WANT IT IN THE ARRAY
-                    WordAnalysis::getUniqueWordCount(uniqueWord);
-                    wordCount++;
+                //now check if already in the array
+                bool alreadyInList = false;
+                for(int i = 0; i < index; i++)
+                {
+                    if(words[i].w == uniqueWord)
+                    {
+                        alreadyInList = true;
+                        words[i].count++;
+                    }
                 }
-                //arraydoubling goes here
+                if(!alreadyInList)
+                {
+                    doubleArrayAndAdd(uniqueWord);
+                    index++;
+                }
+                }
             }
         }
+        sortData();
+        return true;
 
     }
     
     else{
         cout << "file didn't open"<< endl;
-        fileRead = false;
+        return false;
     }
-    WordAnalysis::sortData(<#word *uniqueWords#>)
-    return fileRead;
 }
 
+WordAnalysis::WordAnalysis(int zane){
+    words = new word[100];
+    wordCount = 0;
+    index = 0;
+    timesDoubled = 0;
+};
+
+WordAnalysis::~WordAnalysis(){
+    delete[] words;
+};
 int main(int argc, const char * argv[]) {
-    // insert code here...
     string fName = "";
-    char* fileN = fName; 
     int numberOfMostFrequentWords;
+    cout << "Please enter your filename."<< endl;//opening statement
     getline(cin, fName); // allows the user to input the filename
     cin >> numberOfMostFrequentWords; // takes input on the number of most frequent words
-    readDataFile(fName);
+    int defaultsize = 100;
+    WordAnalysis wa(defaultsize);
+    char* fileN = (char*)fName.c_str();
+    wa.readDataFile(fileN);
+    wa.printCommonWords(numberOfMostFrequentWords);
+    wa.printResult(numberOfMostFrequentWords);
     return 0;
 }
